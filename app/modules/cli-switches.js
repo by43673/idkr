d4e1507f166f9a9f68703e53ef99308ede4e23c9
by43673 <121001726+by43673@users.noreply.github.com>
2024-10-16 20:54:03 -0700

    Update cli-switches.js

--- app/modules/cli-switches.js
+++ app/modules/cli-switches.js
@@ -10,49 +10,46 @@ let yargs = require("yargs");
  * @param {import("electron-store")} config
  */
 let cliSwitchHandler = function(app, config) {
-    const flags = [
-        ['disable-frame-rate-limit', null, config.get('unlimitedFPS', true)],
-        ['disable-gpu-vsync', null, config.get('unlimitedFPS', true)],
-        ['max-gum-fps', '9999', config.get('unlimitedFPS', true)],
-        ['disable-features', 'UsePreferredIntervalForVideo', config.get('unlimitedFPS', true)],
-		['use-cmd-decoder', 'passthrough', config.get('unlimitedFPS', true)],
-		['enable-features', 'BlinkCompositorUseDisplayThreadPriority', config.get('unlimitedFPS', true)],
-		['enable-features', 'BrowserUseDisplayThreadPriority', config.get('unlimitedFPS', true)],
-		['enable-features', 'GpuUseDisplayThreadPriority', config.get('unlimitedFPS', true)],
-        ['use-angle', config.get('angleType', 'default'), true],
-        ['in-process-gpu', null, config.get('inProcessGPU', true)],
-        ['autoplay-policy', 'no-user-gesture-required', config.get('autoPlay', true)],
-        ['disable-accelerated-2d-canvas', 'true', !config.get('acceleratedCanvas', true)],
-    ];
+	const angleBackend = /** @type {string} */ (config.get("angleBackend", "default"));
+	const colorProfile = /** @type {string} */ (config.get("colorProfile", "default"));
 
-    // Apply all the flags
-    flags.forEach(([flag, value, condition]) => {
-        if (condition) {
-            if (value) {
-                app.commandLine.appendSwitch(flag, value);
-            } else {
-                app.commandLine.appendSwitch(flag);
-            }
-        }
-    });
+	app.commandLine.appendSwitch("disable-frame-rate-limit");
+		app.commandLine.appendSwitch("disable-gpu-vsync");
+		app.commandLine.appendSwitch('disable-breakpad');
+		app.commandLine.appendSwitch('disable-print-preview');
+		app.commandLine.appendSwitch('disable-metrics-repo');
+		app.commandLine.appendSwitch('disable-metrics');
+		app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
+		app.commandLine.appendSwitch('disable-bundled-ppapi-flash');
+		app.commandLine.appendSwitch('disable-logging');
+		app.commandLine.appendSwitch('disable-hang-monitor');
+		app.commandLine.appendSwitch('disable-component-update');
+		app.commandLine.appendSwitch('enable-webgl2-compute-context');
+		app.commandLine.appendSwitch('enable-future-v8-vm-features');
+		app.commandLine.appendSwitch('disable-background-timer-throttling');
+		app.commandLine.appendSwitch('disable-renderer-backgrounding');
+		app.commandLine.appendSwitch('enable-features', 'BlinkCompositorUseDisplayThreadPriority');
+		app.commandLine.appendSwitch('enable-features', 'GpuUseDisplayThreadPriority');
+		app.commandLine.appendSwitch('enable-features', 'BrowserUseDisplayThreadPriority');
+		app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+		app.commandLine.appendSwitch('disable-features', 'UserInteractiveCompositingMac');
+		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
+		app.commandLine.appendSwitch('disable-features', 'TextureLayerSkipWaitForActivation'); 
 
-    const angleBackend = /** @type {string} */ (config.get("angleBackend", "default"));
-    const colorProfile = /** @type {string} */ (config.get("colorProfile", "default"));
+	if (!config.get("acceleratedCanvas", true)) app.commandLine.appendSwitch("disable-accelerated-2d-canvas", "true");
+	if (config.get("disableFrameRateLimit", true)) {
+		app.commandLine.appendSwitch("disable-frame-rate-limit");
+		app.commandLine.appendSwitch("disable-gpu-vsync");
+	}
+	if (config.get("inProcessGPU", false)) app.commandLine.appendSwitch("in-process-gpu");
+	if (angleBackend !== "default") app.commandLine.appendSwitch("use-angle", angleBackend);
+	if (colorProfile !== "default") app.commandLine.appendSwitch("force-color-profile", colorProfile);
 
-    if (angleBackend !== "default") {
-        app.commandLine.appendSwitch("use-angle", angleBackend);
-    }
-
-    if (colorProfile !== "default") {
-        app.commandLine.appendSwitch("force-color-profile", colorProfile);
-    }
-
-    // Apply custom chromium flags via yargs
-    yargs.parse(
-        /** @type {string} */
-        (config.get("chromiumFlags", "")),
-        (_, argv) => Object.entries(argv).slice(1, -1).forEach(entry => app.commandLine.appendSwitch(entry[0], entry[1]))
-    );
+	yargs.parse(
+		/** @type {string} */
+		(config.get("chromiumFlags", "")),
+		(_, argv) => Object.entries(argv).slice(1, -1).forEach(entry => app.commandLine.appendSwitch(entry[0], entry[1]))
+	);
 };
 
 module.exports = cliSwitchHandler;
